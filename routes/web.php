2d31939bc0fea84b0ee6e8f9a95b2e62a7e28c95
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-07-16 17:13:53 +0200

    * Routes updated

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ Route::get('/blog', [ArticleController::class, 'blog'])->name('blog.articles');
 Route::get('/contact', [ContactController::class, 'index'])->name('blog.contact');
 Route::get('/logout', [LoginController::class, 'logout'])->name('blog.logout');
 
-Route::post('/comment', [CommentController::class, 'store'])->name('comment.store');
 Route::get('/comment/show/{articleId}', [CommentController::class, 'show'])->name('comment.show');
 
 Route::get('/dashboard', function () {
@@ -27,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('/comment', [CommentController::class, 'store'])->name('comment.store');
 });
 
 Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('admin')->group(function () {
